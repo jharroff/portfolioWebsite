fe31d1c42a0a33dec9f8cb67ce405685d8a0d619
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2026-01-09 17:17:13 -0500

    Added Project Image Support

--- src/pages/projects/ProjectShowcaseBox.jsx
+++ src/pages/projects/ProjectShowcaseBox.jsx
@@ -3,7 +3,15 @@ function ProjectShowcaseBox({data}) {
 
   return (
     <>
-      <a class="showcaseDiv" href={data.link} target="_blank">
+      <a className="showcaseDiv" href={data.link} target="_blank">
+        <img 
+          className="showcaseProjectImage" 
+          src={"src/assets/projectImages/" + data.id + ".png"} 
+          onError={({ currentTarget }) => {
+            currentTarget.onerror = null; // prevents looping
+            currentTarget.src="src/assets/imageNotFound.svg";
+          }}>
+        </img>
         <h2>{data.title}</h2>
         <p>{data.description}</p>
       </a>
