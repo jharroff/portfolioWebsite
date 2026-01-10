55326fd402a61856090961cbbe03f17c4109724d
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2026-01-10 16:18:41 -0500

    Changed to HashRouter for githubPages

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import Navbar from './navbar/Navbar.jsx'
 import "./index.css"
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
