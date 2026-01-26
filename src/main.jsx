ee4c88675173f777137d13b2b4fc990244518c35
BigWing06 <166898297+BigWing06@users.noreply.github.com>
2026-01-26 01:16:05 -0500

    Changed Themed Color

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import Navbar from './navbar/Navbar.jsx'
 import "./index.css"
 import App from './App.jsx'
 import { HashRouter, Routes, Route } from "react-router-dom"
+import "./globalStyles.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
+      <img className="backgroundImage" src={import.meta.env.BASE_URL + "/background.webp"}></img>
       <Navbar />
       <App />
     </HashRouter>
