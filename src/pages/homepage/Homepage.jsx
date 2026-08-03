43e5eec0006e87125e47fdec68ed1e49233c44ac
Joshua Harroff <166898297+jharroff@users.noreply.github.com>
2026-08-03 01:07:11 -0400

    Fixed Hover Animation

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -7,47 +7,57 @@ function Homepage() {
   return (
     <div style={{"margin": "25px"}}>
       <div className={styles.rowContainer}>
-        <div className={styles.showcaseDiv + " " + styles.gridAreaBio}>
-          <div className={styles.gridContainer}>
-            <h2 style={{"padding":"0px"}}>About Me</h2>
-            <p className={styles.aboutText}>Hello! I'm Joshua Harroff, a current Purdue student and aspiring Software Engineer. I'm a full stack developer who enjoys exploring and learning new things. Outside of CS, I enjoy traveling, biking, playing board games, and random adventures.</p>
-          </div>
-        </div>
-        <div className={styles.showcaseDiv + " " + styles.gridAreaTech}>
-          <div className={styles.gridContainer}>
-            <h2>Skills</h2>
-            <div className={styles.showcaseContainer}>
-              {["Java", "Python", "Git", "C", "Godot", "Linux", "React"].map(p => (
-                <ShowcaseBox 
-                  name={p} 
-                  imagePath={import.meta.env.BASE_URL + "skillsIcons/" + p + ".svg"} 
-                  tooltip={true}/>
-              ))}
+        <div className={styles.gridAreaBio}>
+          <div className={styles.showcaseDiv}>
+            <div className={styles.gridContainer}>
+              <h2 style={{"padding":"0px"}}>About Me</h2>
+              <p className={styles.aboutText}>Hello! I'm Joshua Harroff, a current Purdue student and aspiring Software Engineer. I'm a full stack developer who enjoys exploring and learning new things. Outside of CS, I enjoy traveling, biking, playing board games, and random adventures.</p>
             </div>
           </div>
         </div>
-        <div className={styles.showcaseDiv + " " + styles.gridAreaImage}>
-          <div className={styles.gridContainer}>
-            <img className={styles.profileImage} src={import.meta.env.BASE_URL + "profilePic.JPG"}></img>
+        <div className={styles.gridAreaTech}>
+          <div className={styles.showcaseDiv}>
+            <div className={styles.gridContainer}>
+              <h2>Skills</h2>
+              <div className={styles.showcaseContainer}>
+                {["Java", "Python", "Git", "C", "Godot", "Linux", "React"].map(p => (
+                  <ShowcaseBox 
+                    name={p} 
+                    imagePath={import.meta.env.BASE_URL + "skillsIcons/" + p + ".svg"} 
+                    tooltip={true}/>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
-        <div className={styles.showcaseDiv + " " + styles.gridAreaGithub}>
-          <div className={styles.gridContainer}>
-            <img className={styles.centerImage} src={import.meta.env.BASE_URL + "/favicon.png"}></img>
-            <h2>Joshua Harroff</h2>
+        <div className={styles.gridAreaImage}>
+          <div className={styles.showcaseDiv}>
+            <div className={styles.gridContainer}>
+              <img className={styles.profileImage} src={import.meta.env.BASE_URL + "profilePic.JPG"}></img>
+            </div>
           </div>
         </div>
-        <div className={styles.showcaseDiv + " " + styles.gridAreaLinks}>
-          <div className={styles.gridContainer}>
-            <h2>Useful Links</h2>
-            <div className={styles.showcaseContainer}>
-              {links.map(link => (
-                <ShowcaseBox 
-                  name={link.name} 
-                  imagePath={import.meta.env.BASE_URL + "linkIcons/" + link.name + ".svg"} 
-                  link={link.link} 
-                  tooltip={true}/>
-              ))}
+        <div className={styles.gridAreaGithub}>
+          <div className={styles.showcaseDiv}>
+            <div className={styles.gridContainer}>
+              <img className={styles.centerImage} src={import.meta.env.BASE_URL + "/favicon.png"}></img>
+              <h2>Joshua Harroff</h2>
+            </div>
+          </div>
+        </div>
+        <div className={styles.gridAreaLinks}>
+          <div className={styles.showcaseDiv}>
+            <div className={styles.gridContainer}>
+              <h2>Useful Links</h2>
+              <div className={styles.showcaseContainer}>
+                {links.map(link => (
+                  <ShowcaseBox 
+                    name={link.name} 
+                    imagePath={import.meta.env.BASE_URL + "linkIcons/" + link.name + ".svg"} 
+                    link={link.link} 
+                    tooltip={true}/>
+                ))}
+              </div>
             </div>
           </div>
         </div>
