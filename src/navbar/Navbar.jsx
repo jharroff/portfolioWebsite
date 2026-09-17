1fe98929f32dd3584e60d234154b22285bb9799b
J Harroff <166898297+jharroff@users.noreply.github.com>
2026-09-17 16:41:41 -0400

    Updated Navbar

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -4,14 +4,38 @@ import './Navbar.css'
 function Navbar() {
 
   return (
-    <div className="navbarDiv">
-      <ul>
-        <NavbarButton pageTitle={"Home"} link={"/"} />
-        <NavbarButton pageTitle={"Projects"} link={"/projects"} />
-        <NavbarButton pageTitle={"Resume"} link={"/resume"} />
-      </ul>
+    <div className="outerNavDiv">
+      <div className="navbarDiv">
+        <div className="nameDiv">
+          <p className={"nameText"}>Joshua Harroff</p>
+        </div>
+        <div className="navLinksDiv">
+          <ul>
+            <NavbarButton pageTitle={"Home"} link={"/"} />
+            <NavbarButton pageTitle={"Projects"} link={"/projects"} />
+            <NavbarButton pageTitle={"Resume"} link={"/resume"} />
+          </ul>
+        </div>
+        <div className="socialLinksDiv">
+          <NavbarExternalLink pageIcon={"LinkedIn"} link={"https://www.linkedin.com/in/joshua-harroff/"} />
+          <NavbarExternalLink pageIcon={"GitHub"} link={"https://github.com/jharroff"} />
+          <NavbarExternalLink pageIcon={"Email"} link={"mailto:joshuaharroff@outlook.com"} />
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Navbar
+
+function NavbarExternalLink({pageIcon, link}) {
+
+  return (
+    <>
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <img className="socialIcon" src={`/public/linkIcons/${pageIcon}.png`} alt={pageIcon} />
+        <div className={("underlineDiv")}></div>
+      </a>
+    </>
+  )
+}
