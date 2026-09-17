ad87b253e2ff7e7e9b83aa6df1d7c9855af7d090
J Harroff <166898297+jharroff@users.noreply.github.com>
2026-09-17 01:08:43 -0400

    Fixed Resume Height

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 
 function Resume() {
 
-  return (<><p></p><div style={{textAlign: 'center', height: '88%'}}><a style={{color: 'white'}} href="/Resume.pdf" target="_blank" rel="noopener noreferrer">Click here to open in a new tab</a><p></p><iframe src="/Resume.pdf" width="95%" height="100%" /></div></>);
+  return (<><p></p><div style={{textAlign: 'center', height: '85%'}}><a style={{color: 'white'}} href="/Resume.pdf" target="_blank" rel="noopener noreferrer">Click here to open in a new tab</a><p></p><iframe src="/Resume.pdf" width="95%" height="100%" /></div></>);
 
 }
 
